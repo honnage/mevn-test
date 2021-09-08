2462d137c42647fecc19793caa382252aebb22e0
unknown <hon_da-1998@hotmail.com>
2021-09-08 22:01:19 +0700

    create form

--- src/components/CreateComponent.vue
+++ src/components/CreateComponent.vue
@@ -2,6 +2,23 @@
     <div class="row justify-content-center">
         <div class="col-md-6">
             <h1>Create Student</h1>
+            <form action="">
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="text" class="form-control" v-model="student.name" required>
+                </div>
+                 <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" class="form-control" v-model="student.email" required>
+                </div>
+                 <div class="form-group">
+                    <label for="phone">Phone</label>
+                    <input type="text" class="form-control" v-model="student.phone" required>
+                </div>
+                <div class="form-group">
+                    <button class="btn btn-success btn-block">Create</button>
+                </div>
+            </form>
         </div>
     </div>
 </template>
@@ -10,8 +27,15 @@
 export default {
     data(){
         return{
-
+            student:{
+                name: '',
+                email: '',
+                phone: ''
+            }
         }
+    },
+    methods:{
+        handleSubmitForm(){}       
     }
 }
 </script>
